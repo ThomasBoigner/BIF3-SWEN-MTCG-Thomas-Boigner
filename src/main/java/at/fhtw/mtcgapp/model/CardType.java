2d925b4018d882d22565e1574d008a93977c030a
Thomas Boigner <thomasboigner@outlook.com>
2024-11-15 18:27:09 +0100

    Add CardType, DamageType, Package and Trade

---
+++ src/main/java/at/fhtw/mtcgapp/model/CardType.java
@@ -0,0 +1,6 @@
+package at.fhtw.mtcgapp.model;
+
+public enum CardType {
+    MONSTER,
+    SPELL
+}
---
+++ src/main/java/at/fhtw/mtcgapp/model/DamageType.java
@@ -0,0 +1,7 @@
+package at.fhtw.mtcgapp.model;
+
+public enum DamageType {
+    FIRE,
+    WATER,
+    NORMAL
+}
